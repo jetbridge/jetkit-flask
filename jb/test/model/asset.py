25b3b7dcdcce6d7f32d4acec6f7a0201888eb9ff
itiievsk <itiyevskiy@gmail.com>
2019-05-15 16:15:00 +0300

    correct models

--- jb/test/model/asset.py
+++ jb/test/model/asset.py
@@ -5,5 +5,5 @@ from sqlalchemy import Integer, ForeignKey, Column
 
 class Asset(CoreAsset):
     __tablename__ = 'asset'
-    owner_id = Column(Integer, ForeignKey('person.id', name="owner_user_fk", use_alter=True), nullable=True)
-    owner = relationship('User', back_populates='assets', foreign_keys=[owner_id], uselist=False)
+    owner_id = Column(Integer, ForeignKey('user.id', name="owner_user_fk", use_alter=True), nullable=True)
+    owner = relationship('jb.test.model.user.User', back_populates='assets', foreign_keys=[owner_id], uselist=False)
