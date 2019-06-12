9209c1494a909b0a871c2dd48d13599af562bb94
Mischa Spiegelmock <mischa@mvstg.biz>
2019-06-12 16:05:43 +0300

    fix refresh token to use POST

--- jb/api/auth/__init__.py
+++ jb/api/auth/__init__.py
@@ -1,15 +1,17 @@
 from flask_rest_api import Blueprint, abort
 from marshmallow import fields as f, Schema
-from flask_jwt_extended import jwt_required, current_user, jwt_refresh_token_required, create_access_token, create_refresh_token
+from flask_jwt_extended import (
+    jwt_required,
+    jwt_refresh_token_required,
+    create_access_token,
+    create_refresh_token,
+    get_current_user,
+)
 from abc import abstractmethod
 from sqlalchemy.orm import Query
 from jb.api.user.schema import UserSchema
 
-blp = Blueprint(
-    'Authentication',
-    __name__,
-    url_prefix='/api/auth',
-)
+blp = Blueprint("Authentication", __name__, url_prefix="/api/auth")
 
 
 # default schemas
@@ -23,7 +25,7 @@ class RefreshTokenResponse(Schema):
 
 
 # auth model protocol
-class AuthModel():
+class AuthModel:
     id: int
     email: str
     password: str
@@ -36,19 +38,13 @@ class AuthModel():
         raise NotImplementedError()
 
 
-def refresh_token_response(user: AuthModel) -> dict:
-    access_token = create_access_token(identity=user)
-    refresh_token = create_refresh_token(identity=user)
-    return {'access_token': access_token, 'refresh_token': refresh_token}
-
-
 def auth_response_for_user(user: AuthModel) -> dict:
     access_token = create_access_token(identity=user)
     refresh_token = create_refresh_token(identity=user)
     return {
-        'access_token': access_token,  # valid for 15 minutes by default
-        'refresh_token': refresh_token,  # valid for 30 days by default
-        'user': user,
+        "access_token": access_token,  # valid for 15 minutes by default
+        "refresh_token": refresh_token,  # valid for 30 days by default
+        "user": user,
     }
 
 
@@ -58,29 +54,26 @@ def CoreAuthAPI(auth_model: AuthModel, user_schema: Schema = UserSchema):
         refresh_token = f.String(dump_only=True)
         user = f.Nested(user_schema)
 
-    @blp.route('login', methods=['POST'])
+    @blp.route("login", methods=["POST"])
     @blp.response(AuthResponse)
     @blp.arguments(LoginRequest, as_kwargs=True)
     def login(email: str, password: str):
         """Login with email + password."""
         cleaned_email = email.strip().lower()
-        user: AuthModel = auth_model.query\
-                                    .filter_by(email=cleaned_email).one_or_none()
+        user: AuthModel = auth_model.query.filter_by(email=cleaned_email).one_or_none()
         if not user or not user.password or not user.is_correct_password(password):
             abort(401, message="Wrong user name or password")
         return auth_response_for_user(user)
 
-    @blp.route('check', methods=['GET'])
+    @blp.route("check", methods=["GET"])
     @jwt_required
     def check_user():
         """Check if current access token is valid."""
         return "ok"
 
-    # endpoint is reachable only if refresh_token is present and valid
-    # access_token itself doesn't provide access to this endpoint
-    @blp.route('refresh', methods=['GET'])
+    @blp.route("refresh", methods=["POST"])
     @jwt_refresh_token_required
     @blp.response(RefreshTokenResponse)
-    def refresh_tokens():
-        """Generate new tokens if current user has valid refresh token"""
-        return refresh_token_response(current_user)
+    def refresh_token():
+        current_user = get_current_user()
+        return {"access_token": create_access_token(identity=current_user)}
