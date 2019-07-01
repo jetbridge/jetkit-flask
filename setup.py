71dd72714c5d5ac297bf2678c807f99d701c5e08
Mischa Spiegelmock <mischa@mvstg.biz>
2019-07-01 11:26:34 +0300

    setup.py updates

--- setup.py
+++ setup.py
@@ -1,34 +1,30 @@
 from setuptools import setup, find_packages
 
-with open('README.md') as f:
+with open("README.md") as f:
     long_description = f.read()
 
-NAME = 'jb'
-DESCRIPTION = 'Common reusable code for python projects.'
-VERSION = '1.1.0'
-REQUIRES_PYTHON = '>=3.6.0'
+NAME = "jb"
+DESCRIPTION = "Common reusable code for python projects."
+VERSION = "1.1.0"
+REQUIRES_PYTHON = ">=3.6.0"
 
 setup(
     name=NAME,
     version=VERSION,
-    url='https://github.com/jetbridge/backend-core',
-    license='ABRMS',
-    author='JetBridge',
-    author_email='mischa@jetbridge.com',
+    python_requires=REQUIRES_PYTHON,
+    url="https://github.com/jetbridge/backend-core",
+    license="ABRMS",
+    author="JetBridge",
+    author_email="mischa@jetbridge.com",
     description=DESCRIPTION,
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     # py_modules=['jb'],
     # if you would be using a package instead use packages instead
     # of py_modules:
     packages=find_packages(exclude=["test", "*.test", "*.test.*", "test.*"]),
     zip_safe=False,
     include_package_data=True,
-    platforms='any',
-    install_requires=[
-        'Flask',
-        'boto3',
-        'sqlalchemy',
-        'requests',
-    ],
+    platforms="any",
+    install_requires=["Flask", "boto3", "sqlalchemy", "requests"],
 )
