5ec4a73fe33bcfb2425469ca25ce6a54488777a8
Mischa Spiegelmock <mischa@mvstg.biz>
2019-07-02 15:29:22 +0300

    deleted column moved to softdeletable

--- jb/db/__init__.py
+++ jb/db/__init__.py
@@ -12,7 +12,6 @@ class BaseModel(object):
     id = Column(Integer, primary_key=True)
     created = Column(TSTZ, nullable=False, server_default=func.now())
     updated = Column(TSTZ, nullable=True, onupdate=func.now())
-    deleted = Column(TSTZ, nullable=True)
 
     def update(self, **kwargs):
         for attr, value in kwargs.items():
@@ -25,19 +24,21 @@ Model = db.Model
 
 
 # model mixin
-class Upsertable():
+class Upsertable:
     """Model mixin to provide postgres upsert capability."""
 
     @classmethod
-    def upsert_row(cls,
-                   row_class,
-                   *,
-                   session,
-                   index_elements: List[str] = None,
-                   constraint=None,
-                   set_: Dict[str, Any],
-                   should_return_result=True,
-                   values: Dict[str, Any]):
+    def upsert_row(
+        cls,
+        row_class,
+        *,
+        session,
+        index_elements: List[str] = None,
+        constraint=None,
+        set_: Dict[str, Any],
+        should_return_result=True,
+        values: Dict[str, Any],
+    ):
         """Insert or update if index_elements match.
 
         N.B. does not commit.
@@ -50,16 +51,17 @@ class Upsertable():
         # what do we detect conflict on?
         conflict = None
         if index_elements:
-            conflict = {'index_elements': index_elements}
+            conflict = {"index_elements": index_elements}
         elif constraint:
-            conflict = {'constraint': constraint}
+            conflict = {"constraint": constraint}
         else:
             raise Exception("constraint or index_elements must be specified")
 
-        insert_query = pg_insert(row_class).on_conflict_do_update(
-            **conflict,
-            set_=set_,
-        ).values(**values)
+        insert_query = (
+            pg_insert(row_class)
+            .on_conflict_do_update(**conflict, set_=set_)
+            .values(**values)
+        )
         res = session.execute(insert_query)
         if not should_return_result:
             return None
