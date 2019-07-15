0f6214432033344e251e6b9a9a1d6e8f1f92440a
Adam Szeptycki <adamszeptycki@gmail.com>
2019-07-15 09:47:29 +0200

    add a way to request uplaod url

--- jb/model/asset.py
+++ jb/model/asset.py
@@ -1,7 +1,9 @@
 """Asset model - files stored on AWS S3."""
 import base64
 import enum
+from typing import Optional
 
+from sqlalchemy import func
 from sqlalchemy.ext.declarative import declared_attr
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql.schema import Column, ForeignKey
@@ -16,11 +18,14 @@ import re
 import logging
 import botocore.exceptions
 from furl import furl
-from jb.db import db, BaseModel
+
+from jb.aws import get_session
+from jb.db import db, BaseModel, TSTZ
 
 log = logging.getLogger(__name__)
 
 
+@enum.unique
 class AssetStatus(enum.Enum):
     added = "added"
     in_progress = "in_progress"
@@ -28,9 +33,20 @@ class AssetStatus(enum.Enum):
     errored = "errored"
 
 
+@enum.unique
+class ACL(enum.Enum):
+    """Who can view this asset.
+
+    Set on S3 object."""
+
+    private = "private"
+    public_read = "public-read"
+
+
 class Asset(BaseModel):
     """Keep a record of files that have been uploaded to S3."""
 
+    updated = Column(TSTZ, nullable=True, onupdate=func.now())
     s3bucket = Column(Text, nullable=False)
     s3key = Column(Text, nullable=False)
 
@@ -48,117 +64,35 @@ class Asset(BaseModel):
     @declared_attr
     def owner(self):
         return relationship('User', foreign_keys=[self.user_id], uselist=False)
-    
-    def check_main_type(self, expected_type):
-        """Check if main mime type is equal to expected type."""
-        if not self.mime_type:
-            return False
-        mimetype_parts = self.mime_type.split("/")
-        if mimetype_parts:
-            return mimetype_parts[0] == expected_type
-        return False
 
-    def is_video(self):
-        """Return true iff asset is video."""
-        return self.check_main_type(expected_type="video")
+    @classmethod
+    def process_event_record(cls, record: dict):
+        assert "s3" in record
 
-    def is_image(self):
-        """Return true iff asset is image."""
-        return self.check_main_type(expected_type="image")
+        # look up asset by key/bucket
+        s3 = record["s3"]
+        s3key = s3["object"]["key"]
+        s3bucket = s3["bucket"]["name"]
 
-    def is_pdf(self):
-        """Return true iff asset is pdf."""
-        return self.mime_type == "application/pdf"
+        asset = (
+            db.session.query(Asset).filter_by(s3key=s3key, s3bucket=s3bucket).one_or_none()
+        )
+        if not asset:
+            log.error(
+                f"Got S3 event for object {s3key} in {s3bucket} but no asset row was found."
+            )
+            # do nothing
+            return None
 
-    def user_can_read(self, user):
-        """Check if a user can view this asset.
+        asset.update_from_upload(s3["object"])
+        db.session.commit()
 
-        Make sure this is implemented properly for all possible asset types so that
-        people cannot enumerate assets.
-        """
-        # more cases go here...
-        if not user:  # anonymous user
-            # can access asset unowned asset from public bucket
-            is_unowned = self.user_id is None
-            is_in_public_bucket = self.s3bucket in current_app.config.get("S3_PUBLIC_BUCKETS", [])
-            return is_unowned and is_in_public_bucket
-
-        return user.id == self.user_id
-
-    def user_can_write(self, user):
-        """Asset creator can update asset."""
-        return user.id == self.user_id
+        return asset
 
     @classmethod
     def filter_query_for_user(cls, query, user):
         """List only assets created by user."""
         return query.filter(cls.user_id == user.id)
-
-    def base64_encoded_external_link(self):
-        """Return base64 encoded external link."""
-        external_link = self.external_link()
-        return base64.b64encode(external_link.encode()).decode()
-
-    def external_link(self, email=None):
-        """Return external link to access this asset."""
-        return self.s3_view_url()
-
-    def s3_view_url(self):
-        """Generate presigned URL to view this asset."""
-        assert self.s3bucket, "s3_view_url() called on asset with no s3bucket set"
-        assert self.s3key, "s3_view_url() called on asset with no s3key set"
-        extra = {}
-        if self.mime_type:
-            extra["content_type"] = self.mime_type
-        return s3.get_presigned_view_url(self.s3bucket, self.s3key, **extra)
-
-    def s3_direct_url(self):
-        """Generate S3 URL, assumes this is viewable by the world."""
-        return furl(
-            scheme="https", host=f"{self.s3bucket}.s3.amazonaws.com/{self.s3key}"
-        )
-
-    @classmethod
-    def _generate_s3_key(cls, directory, file_name, content_type):
-        """Hash everything together and hope we get a unique S3key."""
-        assert directory
-
-        h = hashlib.sha256()
-        h.update(str(random.random()).encode("utf-8"))  # bleh
-        h.update(current_app.config["SECRET_KEY"].encode("utf-8"))
-
-        key_name = "{}_original".format(h.hexdigest())
-
-        # add extension to our keyname if we got it
-        extension = None
-        if file_name:
-            # use uploaded file extension
-            extension = os.path.splitext(file_name)[1]
-            # slugify name and prefix it
-            file_name_slug = re.sub(r"[^A-Za-z0-9]", "-", file_name)
-            key_name = file_name_slug + "_" + key_name
-        if not extension and content_type:
-            # or try to guess from mime type
-            extension = mimetypes.guess_extension(content_type)
-        if extension:
-            key_name = key_name + extension
-
-        key_path = os.path.join(directory, key_name)
-
-        # make sure this key is unique (it really really should be)
-        existing_asset = cls.query.filter_by(s3key=key_path).one_or_none()
-        assert not existing_asset, "got highly unexpected s3 key collision!"
-        return key_path
-
-    @classmethod
-    def get_asset_put_url(cls, directory, file_name, content_type, bucket_name=None):
-        """Generate presigned S3 upload URL.
-
-        :returns: URL, s3key
-        """
-        s3key = cls._generate_s3_key(directory, file_name, content_type)
-        url = s3.get_presigned_put(s3key, content_type, bucket_name=bucket_name)
-        return url, s3key
 
     @classmethod
     def create_asset_from_flask_upload(cls, directory, upload, owner):
@@ -175,7 +109,7 @@ class Asset(BaseModel):
         return asset
 
     @classmethod
-    def create_from_content(cls, *, user, content, content_type, directory=None, s3key: str=None):
+    def create_from_content(cls, *, user, content, content_type, directory=None, s3key: str = None):
         """Copy file to S3 and return asset."""
         if not s3key:  # get S3 key
             s3key = cls._generate_s3_key(directory=directory, file_name='raw', content_type=content_type)
@@ -189,13 +123,13 @@ class Asset(BaseModel):
 
     @classmethod
     def create_asset(
-        cls,
-        s3key,
-        content_type,
-        owner=None,
-        filename=None,
-        status=None,
-        bucket_name=None,
+            cls,
+            s3key,
+            content_type,
+            owner=None,
+            filename=None,
+            status=None,
+            bucket_name=None,
     ):
         """Create an asset record for an S3 file.
 
@@ -259,19 +193,47 @@ class Asset(BaseModel):
         db.session.commit()
         return asset
 
-    def get_hash(self) -> str:
-        """Get security hash for preventing enumeration."""
-        m = hashlib.sha256()
-        m.update(current_app.config["SECRET_KEY"].encode("utf-8"))
-        m.update(str(self.id).encode("utf-8"))
-        hex_ = m.hexdigest()
-        return hex_
+    @classmethod
+    def _generate_s3_key(cls, directory, file_name, content_type):
+        """Hash everything together and hope we get a unique S3key."""
+        assert directory
 
-    def file_name_without_type(self):
-        """Return file name without type."""
-        parts = self.filename.split(".") if self.filename else []
-        if parts:
-            return ".".join(parts)
+        h = hashlib.sha256()
+        h.update(str(random.random()).encode("utf-8"))  # bleh
+        h.update(current_app.config["SECRET_KEY"].encode("utf-8"))
+
+        key_name = "{}_original".format(h.hexdigest())
+
+        # add extension to our keyname if we got it
+        extension = None
+        if file_name:
+            # use uploaded file extension
+            extension = os.path.splitext(file_name)[1]
+            # slugify name and prefix it
+            file_name_slug = re.sub(r"[^A-Za-z0-9]", "-", file_name)
+            key_name = file_name_slug + "_" + key_name
+        if not extension and content_type:
+            # or try to guess from mime type
+            extension = mimetypes.guess_extension(content_type)
+        if extension:
+            key_name = key_name + extension
+
+        key_path = os.path.join(directory, key_name)
+
+        # make sure this key is unique (it really really should be)
+        existing_asset = cls.query.filter_by(s3key=key_path).one_or_none()
+        assert not existing_asset, "got highly unexpected s3 key collision!"
+        return key_path
+
+    @classmethod
+    def get_asset_put_url(cls, directory, file_name, content_type, bucket_name=None):
+        """Generate presigned S3 upload URL.
+
+        :returns: URL, s3key
+        """
+        s3key = cls._generate_s3_key(directory, file_name, content_type)
+        url = s3.get_presigned_put(s3key, content_type, bucket_name=bucket_name)
+        return url, s3key
 
     @classmethod
     def get_asset(cls, asset_id, asset_hash):
@@ -288,3 +250,105 @@ class Asset(BaseModel):
             )
             return None
         return asset
+
+    def check_main_type(self, expected_type):
+        """Check if main mime type is equal to expected type."""
+        if not self.mime_type:
+            return False
+        mimetype_parts = self.mime_type.split("/")
+        if mimetype_parts:
+            return mimetype_parts[0] == expected_type
+        return False
+
+    def is_video(self):
+        """Return true iff asset is video."""
+        return self.check_main_type(expected_type="video")
+
+    def is_image(self):
+        """Return true iff asset is image."""
+        return self.check_main_type(expected_type="image")
+
+    def is_pdf(self):
+        """Return true iff asset is pdf."""
+        return self.mime_type == "application/pdf"
+
+    def user_can_read(self, user):
+        """Check if a user can view this asset.
+
+        Make sure this is implemented properly for all possible asset types so that
+        people cannot enumerate assets.
+        """
+        # more cases go here...
+        if not user:  # anonymous user
+            # can access asset unowned asset from public bucket
+            is_unowned = self.user_id is None
+            is_in_public_bucket = self.s3bucket in current_app.config.get("S3_PUBLIC_BUCKETS", [])
+            return is_unowned and is_in_public_bucket
+
+        return user.id == self.user_id
+
+    def user_can_write(self, user):
+        """Asset creator can update asset."""
+        return user.id == self.user_id
+
+    def base64_encoded_external_link(self):
+        """Return base64 encoded external link."""
+        external_link = self.external_link()
+        return base64.b64encode(external_link.encode()).decode()
+
+    def external_link(self, email=None):
+        """Return external link to access this asset."""
+        return self.s3_view_url()
+
+    def s3_view_url(self):
+        """Generate presigned URL to view this asset."""
+        assert self.s3bucket, "s3_view_url() called on asset with no s3bucket set"
+        assert self.s3key, "s3_view_url() called on asset with no s3key set"
+        extra = {}
+        if self.mime_type:
+            extra["content_type"] = self.mime_type
+        return s3.get_presigned_view_url(self.s3bucket, self.s3key, **extra)
+
+    def s3_direct_url(self):
+        """Generate S3 URL, assumes this is viewable by the world."""
+        return furl(
+            scheme="https", host=f"{self.s3bucket}.s3.amazonaws.com/{self.s3key}"
+        )
+
+    def get_hash(self) -> str:
+        """Get security hash for preventing enumeration."""
+        m = hashlib.sha256()
+        m.update(current_app.config["SECRET_KEY"].encode("utf-8"))
+        m.update(str(self.id).encode("utf-8"))
+        hex_ = m.hexdigest()
+        return hex_
+
+    def file_name_without_type(self):
+        """Return file name without type."""
+        parts = self.filename.split(".") if self.filename else []
+        if parts:
+            return ".".join(parts)
+
+    def generate_presigned_upload(
+        self, content_type: str = None, expire: int = 86400, acl: ACL = ACL.private
+    ):
+        """Get a S3 presigned URL to upload a file via PUT."""
+        headers = {"x-amz-acl": acl.value}
+
+        # PutObject params
+        put_params = dict(Bucket=self.s3bucket, Key=self.s3key, ACL=acl.value)
+        if content_type:
+            put_params["ContentType"] = content_type
+            headers["content-type"] = content_type
+
+        s3 = get_session().client(
+            "s3", endpoint_url=f"https://s3.{self.region}.amazonaws.com"
+        )
+        url = s3.generate_presigned_url(
+            ClientMethod="put_object", Params=put_params, ExpiresIn=expire
+        )
+        return {"url": url, "headers": headers}
+
+    def update_from_upload(self, s3_obj_evt: dict):
+        self.size = s3_obj_evt["size"]
+        self.updated = func.now()
