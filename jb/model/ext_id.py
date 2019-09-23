04f7e15e18873a523c84482bd350cdb47eeef839
Mischa Spiegelmock <mischa@mvstg.biz>
2019-09-23 16:01:01 +0300

    extid-generic

--- jb/model/ext_id.py
+++ jb/model/ext_id.py
@@ -1,30 +1,32 @@
 from sqlalchemy import Column, event, text
 from sqlalchemy.dialects.postgresql import UUID
+from typing import Optional, Generic, TypeVar
+from jb.db import db
 from sqlalchemy.schema import DDL
 
+T = TypeVar("T", bound=db.Model)
 
-class ExtID:
-    """Add an external UUID column.
 
+class ExtID(Generic[T]):
+    """Add an external UUID column `extid`.
+
+    Useful for exposing unique identifiers to clients.
     Requires "uuid-ossp" extension.
     """
 
     # UUID that can be used for semi-secret key
-    ext_id = Column(
+    extid = Column(
         UUID(as_uuid=True), nullable=False, server_default=text("uuid_generate_v4()")
     )
 
     @classmethod
-    def add_create_uuid_extension_trigger(cls, TableClass):
-        """Call this for any tables that use ext_id to ensure they have the uuid-ossp extension already available."""
+    def get_by_extid(cls: T, uuid: str) -> Optional[T]:
+        return cls.query.filter_by(extid=uuid).one_or_none()
 
-        if not hasattr(TableClass, "__table__"):
-            raise Exception(
-                "add_create_uuid_extension_trigger not called on a SQLAlchemy model class"
-            )
+    @classmethod
+    def add_create_uuid_extension_trigger(cls):
+        """Call this for any tables that use ext_id to ensure they have the uuid-ossp extension already available."""
         trigger = DDL('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";')
         event.listen(
-            TableClass.__table__,
-            "before_create",
-            trigger.execute_if(dialect="postgresql"),
+            cls.__table__, "before_create", trigger.execute_if(dialect="postgresql")
         )
