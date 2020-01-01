b2a5b11b3939f7d5085dac3022141f45f12aa830
Mischa Spiegelmock <mischa@mvstg.biz>
2020-01-01 22:55:39 +0200

    add scoped sessionmaker

--- jetkit/db/__init__.py
+++ jetkit/db/__init__.py
@@ -1,6 +1,16 @@
+from flask import current_app
+from sqlalchemy.orm import scoped_session
+
 from jetkit.db.bases import SQLA
 from jetkit.db.model import BaseModel
 from jetkit.db.query import BaseQuery
 
 
-__all__ = ("BaseQuery", "BaseModel", "SQLA")
+# scoped sessionmaker that accesses whatever the current flask-sqlalchemy session is
+Session = scoped_session(
+    lambda: current_app.extensions["sqlalchemy"].db.session,
+    scopefunc=lambda: current_app.extensions["sqlalchemy"].db.session,
+)
+
+
+__all__ = ("BaseQuery", "BaseModel", "SQLA", "Session")
