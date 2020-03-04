78e21e3dbac9bf2dd764445f58e9d76f0f62df61
Dizzzmas <yankovskyi.dmytro736@gmail.com>
2020-03-05 01:28:15 +0200

    Added sign up for auth api

--- jetkit/api/auth/__init__.py
+++ jetkit/api/auth/__init__.py
@@ -11,6 +11,10 @@ from abc import abstractmethod
 from sqlalchemy.orm import Query
 from jetkit.api.user.schema import UserSchema
 from typing import Type
+from jetkit.model.user import CoreUser as User
+from sqlalchemy.dialects.postgresql import insert as pg_insert
+from ...model.user import CoreUserType
+from werkzeug.security import generate_password_hash
 
 blp = Blueprint("Authentication", __name__, url_prefix="/api/auth")
 
@@ -19,6 +23,20 @@ blp = Blueprint("Authentication", __name__, url_prefix="/api/auth")
 class LoginRequest(Schema):
     password = f.String(required=True, allow_none=False)
     email = f.String(required=True, allow_none=False)
+
+
+class SignUpResponse(Schema):
+    extid = f.String(dump_only=True, data_key="id")
+    name = f.String()
+    email = f.Email()
+    dob = f.Date()
+    phone_number = f.String()
+
+
+class AuthResponse(Schema):
+    access_token = f.String()
+    refresh_token = f.String()
+    user = f.Nested(SignUpResponse)
 
 
 class RefreshTokenResponse(Schema):
@@ -39,6 +57,7 @@ class AuthModel:
         raise NotImplementedError()
 
 
+@blp.response(AuthResponse)
 def auth_response_for_user(user: AuthModel) -> dict:
     access_token = create_access_token(identity=user)
     refresh_token = create_refresh_token(identity=user)
@@ -50,13 +69,7 @@ def auth_response_for_user(user: AuthModel) -> dict:
 
 
 def use_core_auth_api(auth_model: AuthModel, user_schema: Type[Schema] = UserSchema):
-    class AuthResponse(Schema):
-        access_token = f.String(dump_only=True)
-        refresh_token = f.String(dump_only=True)
-        user = f.Nested(user_schema)
-
     @blp.route("login", methods=["POST"])
-    @blp.response(AuthResponse)
     @blp.arguments(LoginRequest, as_kwargs=True)
     def login(email: str, password: str):
         """Login with email + password."""
@@ -65,6 +78,24 @@ def use_core_auth_api(auth_model: AuthModel, user_schema: Type[Schema] = UserSch
         if not user or not user.password or not user.is_correct_password(password):
             abort(401, message="Wrong user name or password")
         return auth_response_for_user(user)
+
+    @blp.route("sign-up", methods=["POST"])
+    @blp.response(SignUpResponse)
+    @blp.arguments(LoginRequest, as_kwargs=True)
+    def sign_up(email: str, password: str):
+        """Sign up with email and password. Possibly add other fields later."""
+        cleaned_email = email.strip().lower()
+        existing_user: AuthModel = auth_model.query.filter_by(email=cleaned_email).one_or_none()
+        if existing_user:
+            abort(400, message="There's already a registered user with such email")
+        values = dict(email=email, _password=generate_password_hash(password), _user_type=CoreUserType.normal.value)
+        new_user = User.upsert_row(
+            auth_model,
+            values=values,
+            index_elements=("id", "extid")
+        )
+        auth_model.query.session.commit()
+        return new_user
 
     @blp.route("check", methods=["GET"])
     @jwt_required
